4aad69acc2c24ca236d71d78492fb76982cde57d
Sabatino Masala <Masalasabatino@gmail.com>
2017-10-10 14:09:52 +0200

    example

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import PhotoSwipe from 'vue-simple-photoswipe/dist/vue-simple-photoswipe';
 import Vue from 'vue';
 Vue.use(PhotoSwipe);
+
+import App from './App';
+let $vm = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
