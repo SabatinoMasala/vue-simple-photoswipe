6876a665dcf47f09d1711ab5c8af4026cac962b0
Sabatino Masala <Masalasabatino@gmail.com>
2017-10-10 14:30:41 +0200

    update example

--- nuxt.js
+++
@@ -1,4 +0,0 @@
-import PhotoSwipe from 'vue-simple-photoswipe';
-import Vue from 'vue'
-
-Vue.use(PhotoSwipe);
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import PhotoSwipe from 'vue-simple-photoswipe/dist/vue-simple-photoswipe';
-import Vue from 'vue';
-Vue.use(PhotoSwipe);
-
-import App from './App';
-let $vm = new Vue({
-    el: '#app',
-    render: h => h(App)
-});
