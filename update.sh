bcd4def2429cf575a0875b7f4ba73ac290833665
Sabatino Masala <Masalasabatino@gmail.com>
2017-10-10 14:31:08 +0200

    bump version

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
-yarn release
 yarn build
-yarn publish
 git add . && git commit -m "bump version"
-git push origin master
+git push origin example
 git push origin `git subtree split --prefix dist example`:gh-pages --force
